09c8056e8390a3cd1bcd8b43b73de4756db66510
SynhDrum <otr09@icloud.com>
2025-10-20 16:51:17 -0500

    fixed strafing

--- TeamCode/src/main/java/Winning.java
+++ TeamCode/src/main/java/Winning.java
@@ -41,13 +41,13 @@ public class Winning extends LinearOpMode {
         vx = drift * vx + (1 - drift) * xMove;
         vy = drift * vy + (1 - drift) * yMove;
 
-        double speedDivisor = Math.max(Math.abs(vx) + Math.abs(vy) + Math.abs(steerAngle), 1); //Limits a motor speed from being more than the max (1)
+        //double speedDivisor = Math.max(Math.abs(vx) + Math.abs(vy) + Math.abs(steerAngle), 1); //Limits a motor speed from being more than the max (1)
 
         //Calculate individual motor speeds
-        double frontLeftVel = (vy + vx + steerAngle) / speedDivisor;
-        double backLeftVel = (vy - vx + steerAngle) / speedDivisor;
-        double frontRightVel = (vy - vx - steerAngle) / speedDivisor;
-        double backRightVel = (vy + vx - steerAngle) / speedDivisor;
+        double frontLeftVel = (vy + vx + steerAngle);
+        double backLeftVel = (vy - vx + steerAngle);
+        double frontRightVel = (vy + vx - steerAngle);
+        double backRightVel = (vy - vx - steerAngle);
 
         if(gamepad.x){ //Emergency movement stop
             frontLeftVel = 0;
