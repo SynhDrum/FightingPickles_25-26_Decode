76a8f65bd263f0d0f9a95eccca7e695dfaf0eef9
SynhDrum <otr09@icloud.com>
2025-11-05 08:03:47 -0600

    Added Meep Meep, updated drift code 🔥💯🗣️🗣️ to actually work

--- TeamCode/src/main/java/Winning.java
+++ TeamCode/src/main/java/Winning.java
@@ -1,22 +1,55 @@
 //Winning Code 💯🔥🔥🗣️🗣️
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
 @TeleOp(name="Winning")
 public class Winning extends LinearOpMode {
     ControlHub hub; //Control hub (duh)
 
     double vx = 0; //Velocity x
     double vy = 0; //Velocity y
+    double dir = 0; //Direction of robot
     double steerAngle = 0; //Angle of steering
     double drift = 0; //Amount of drift
+    double dx = 0; //Drift Velocity X
+    double dy = 0; //Drift Velocity Y
+
+    static RevHubOrientationOnRobot.LogoFacingDirection[] logoFacingDirections
+            = RevHubOrientationOnRobot.LogoFacingDirection.values();
+    static RevHubOrientationOnRobot.UsbFacingDirection[] usbFacingDirections
+            = RevHubOrientationOnRobot.UsbFacingDirection.values();
+
+    IMU imu;
+    int logoFacingDirectionPosition;
+    int usbFacingDirectionPosition;
+    boolean orientationIsValid = true;
 
     @Override
     public void runOpMode() throws InterruptedException {
         hub = new ControlHub();
         hub.init(hardwareMap); //Initial position
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        logoFacingDirectionPosition = 0; // Up
+        usbFacingDirectionPosition = 2; // Forward
+
+        RevHubOrientationOnRobot.LogoFacingDirection logo = logoFacingDirections[logoFacingDirectionPosition];
+        RevHubOrientationOnRobot.UsbFacingDirection usb = usbFacingDirections[usbFacingDirectionPosition];
+        try {
+            RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logo, usb);
+            imu.initialize(new IMU.Parameters(orientationOnRobot));
+            orientationIsValid = true;
+        } catch (IllegalArgumentException e) {
+            orientationIsValid = false;
+        }
 
         waitForStart();
         while(opModeIsActive()){ //Main loop
@@ -28,18 +61,29 @@ public class Winning extends LinearOpMode {
         double xMove = gamepad.left_stick_x * 1.1; //Counteract imperfect strafing
         double yMove = -gamepad.left_stick_y; //y stick is reversed
 
+        // Create an object to receive the IMU angles
+        YawPitchRollAngles robotOrientation;
+        robotOrientation = imu.getRobotYawPitchRollAngles();
+
+        dir = robotOrientation.getYaw(AngleUnit.RADIANS);
+
         steerAngle = gamepad.right_stick_x; //Angle to turn by
 
         //Drift mode
         if(gamepad.left_trigger > 0.1){
             drift = 0.95;
+
+            //Interpolate xy speed based on drift
+            dx = drift * dx + (1 - drift) * xMove;
+            dy = drift * dy + (1 - drift) * yMove;
+
+            vx = dx / Math.cos(dir);
+            vy = dy / Math.cos(dir);
         }else{
             drift = 0;
+            vx = xMove;
+            vy = yMove;
         }
-
-        //Interpolate speed based on drift
-        vx = drift * vx + (1 - drift) * xMove;
-        vy = drift * vy + (1 - drift) * yMove;
 
         double speedDivisor = Math.max(Math.max(Math.max(Math.abs(vy + vx + steerAngle), Math.abs(vy - vx + steerAngle)), Math.max(Math.abs(vy + vx - steerAngle), Math.abs(vy - vx - steerAngle))), 1); //Limits all motor speeds from being more than the max (1)
 
